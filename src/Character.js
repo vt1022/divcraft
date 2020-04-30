17b98c4c85759597187c835dcbe21f039ae1f68b
vt1022 <57807628+vt1022@users.noreply.github.com>
2020-04-30 00:01:15 -0400

    working on css features

--- src/Character.js
+++ src/Character.js
@@ -4,20 +4,18 @@ class Character extends Component {
   render() {
     const {head, hair, eyes, nose, mouth, body} = this.props;
     return(
-      <div className="canvas">
-        <div className="character">
-          <div className={head}>
-            <div className={hair}>
-              <div className="left"></div>
-              <div className="right"></div>
-            </div>
-            <div className={eyes}>
-              <div className="left"></div>
-              <div className="right"></div>
-            </div>
-            <div className={nose}></div>
-            <div className={mouth}></div>
+      <div className="character">
+        <div className={head}>
+          <div className={hair}>
+            <div className="left"></div>
+            <div className="right"></div>
           </div>
+          <div className={eyes}>
+            <div className="left"></div>
+            <div className="right"></div>
+          </div>
+          <div className={nose}></div>
+          <div className={mouth}></div>
           <div className={body}></div>
         </div>
       </div>
