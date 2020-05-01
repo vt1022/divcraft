99990ca27db447283c7a82d8ce96b9bda33794c1
vt1022 <57807628+vt1022@users.noreply.github.com>
2020-04-30 23:46:00 -0400

    added features css and logic for saving to firebase ψ(._. )>

--- src/App.js
+++ src/App.js
@@ -11,15 +11,14 @@ class App extends Component {
       characterArray: [],
       userCharacterName: "",
       currentCharacter: {
-        "charId": "01",
-        "charName": "default",
+        "charName": "",
         "charStyles": {
-          "head": "head1",
-          "hair": "hair1",
-          "eyes": "eyes1",
-          "nose": "nose1",
-          "mouth": "mouth1",
-          "body": "body1"
+          "head": "",
+          "hair": "",
+          "eyes": "",
+          "nose": "",
+          "mouth": "",
+          "body": ""
         }
       },
       featuresToShowCss: ["eyes1", "eyes2", "eyes3", "eyes4", "eyes5"]
@@ -35,8 +34,7 @@ class App extends Component {
       for (let key in data) {
         const {body, eyes, hair, head, mouth, nose} = data[key].charStyles
         const characterObject = {
-          "charId": key,
-          "charName": key.charName,
+          "charName": data[key].charName,
           "charStyles": {
             "head": head,
             "hair": hair,
@@ -45,36 +43,35 @@ class App extends Component {
             "mouth": mouth,
             "body": body
           }
+          
         }
         arrayOfCharacters.push(characterObject)
         this.setState({
           characterArray: arrayOfCharacters,
           currentCharacter: characterObject
         })
-        console.log(data)
       }
+      console.log(data)
     })
   }
 
   handleSubmit = (e) => {
     e.preventDefault();
-    // const {character} = this.state;
     if (this.state.userCharacterName.trim() !== "") {
+      const {head, hair, eyes, nose, mouth, body} = this.state.currentCharacter.charStyles
       const dbRef = firebase.database().ref()
-      // push this to fb:
-      // const characterObject = {
-      //   "charName": this.state.userCharacterName,
-      //   "charStyles": {
-      //     "head": head,
-      //     "hair": hair,
-      //     "eyes": eyes,
-      //     "nose": nose,
-      //     "mouth": mouth,
-      //     "body": body
-      //   }
-      // }
-      // change this to push object:
-      dbRef.push(this.state.userCharacterName)
+      const characterObjToFirebase = {
+        "charName": this.state.userCharacterName,
+        "charStyles": {
+          "head": head,
+          "hair": hair,
+          "eyes": eyes,
+          "nose": nose,
+          "mouth": mouth,
+          "body": body
+        }
+      }
+      dbRef.push(characterObjToFirebase)
       this.setState({
         userCharacterName: ""
       })
@@ -108,7 +105,6 @@ class App extends Component {
     const {body, hair, eyes, head, mouth, nose} = this.state.currentCharacter.charStyles
     // change the selected feature
     const newCharObj = {
-      "charId": this.state.currentCharacter.charId,
       "charName": this.state.currentCharacter.charName,
       "charStyles": {
         "head": head,
