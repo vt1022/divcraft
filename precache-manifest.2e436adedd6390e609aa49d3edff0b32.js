5311f2e4104e2652a8a369d19e5fdf0658a2ce5a
vt1022 <57807628+vt1022@users.noreply.github.com>
2020-05-02 22:55:17 -0400

    Updates

---
+++ precache-manifest.2e436adedd6390e609aa49d3edff0b32.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32a94c2486d4dfc65cddae99dfeb360d",
+    "url": "/characterSelection/index.html"
+  },
+  {
+    "revision": "2206780698050645479e",
+    "url": "/characterSelection/static/css/main.2e93a90a.chunk.css"
+  },
+  {
+    "revision": "574255da388a85cab13c",
+    "url": "/characterSelection/static/js/2.de260bce.chunk.js"
+  },
+  {
+    "revision": "4ae2e24854201e2c90f766e681fee893",
+    "url": "/characterSelection/static/js/2.de260bce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2206780698050645479e",
+    "url": "/characterSelection/static/js/main.5a9123cf.chunk.js"
+  },
+  {
+    "revision": "5573818692582336bd22",
+    "url": "/characterSelection/static/js/runtime-main.90b46174.js"
+  }
+]);
